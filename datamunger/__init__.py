96009cb715aa2c6be2b6654e6408d4f164f12f36
p4ndor1c4 <53515244+p4ndor1c4@users.noreply.github.com>
2020-06-08 18:24:47 +0200

    Attempted to fix

--- datamunger/__init__.py
+++
@@ -1,1 +0,0 @@
-from t3020.datamunger import *
--- datamunger/test/test_datamunger.py
+++ datamunger/test/test_datamunger.py
@@ -1,6 +1,7 @@
 import unittest
 import datamunger
 
+
 class TestDatamunger(unittest.TestCase):
     def test_total(self):
         row = [811, 78, 146, 55, 51, 254, 103, 57, 67, 0]
@@ -16,10 +17,12 @@ class TestDatamunger(unittest.TestCase):
 
     def test_row2(self):
         n = 53
-        prev = ['1856', '232', '279', '169', '254', '358', '314', '109', '141', '190']
+        prev = ['1856', '232', '279', '169', '254',
+                '358', '314', '109', '141', '190']
         curr = [1955, 240, 295, 175, 270, 366, 357, 119, 133, 192]
         result = datamunger.check_row(n, prev, curr)
         self.assertEqual(result, False)
+
 
 if __name__ == '__main__':
     unittest.main()
